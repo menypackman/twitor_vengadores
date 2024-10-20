6b81cc68f6bce1c4e2ed764309039d757f3f5178
Manuel Chaparro <meny.chap@gmail.com>
2024-10-20 14:58:40 -0600

    Correción raiz sw

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.addEventListener('install', e => {
 })
 
 self.addEventListener('activate', e => {
-  const respuesta = cache.keys().then( keys => {
+  const respuesta = caches.keys().then( keys => {
     keys.forEach(key => {
       if( key !== STATIC_CACHE && key.includes('static')){
         return caches.delete(key)
