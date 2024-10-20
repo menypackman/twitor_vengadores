31cac17355cc036fb33b534c7f22393a937672f8
Manuel Chaparro <meny.chap@gmail.com>
2024-10-20 14:56:05 -0600

    Correción raiz sw

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const DYNAMIC_CACHE = 'dynamic-v1'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 const APP_SHELL = [
-  '/',
+  // '/',
   'index.html',
   'css/style.css',
   'img/favicon.ico',
@@ -18,8 +18,8 @@ const APP_SHELL = [
 ]
 
 const APP_SHELL_INMUTABLE = [
-  // 'https://fonts.googleapis.com/css?family=Quicksand:300,400',
-  // 'https://fonts.googleapis.com/css?family=Lato:400,300',
+  'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+  'https://fonts.googleapis.com/css?family=Lato:400,300',
   'css/animate.css',
   'js/libs/jquery.js'
 ]
